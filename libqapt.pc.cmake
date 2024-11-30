bf254676786f60d8204175cebe9999af0c45abd4
Elysia <c.elysia@foxmail.com>
2024-11-30 23:59:21 +0800

    Rename to LibQapt6

--- libqapt.pc.cmake
+++ libqapt.pc.cmake
@@ -1,10 +1,10 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@CMAKE_INSTALL_PREFIX@/lib@LIB_SUFFIX@
-includedir=@CMAKE_INSTALL_PREFIX@/include/libqapt
+includedir=@CMAKE_INSTALL_PREFIX@/include/libqapt@PROJECT_VERSION_MAJOR@
 
-Name: libqapt
+Name: libqapt@PROJECT_VERSION_MAJOR@
 Description: Qt wrapper around libapt-pkg
 Version: @QAPT_VERSION_STRING@
-Libs: -L${libdir} -lQApt
+Libs: -L${libdir} -lQApt@PROJECT_VERSION_MAJOR@
 Cflags: -I${includedir}
